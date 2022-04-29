09df77f723f9b5d767b3576ab83d6460488d4e0f
Vignesh <[vamanwani@gmail.com]>
2022-04-29 20:06:20 -0300

    correccion de errores compilacion e indentado

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,16 +1,15 @@
 #include "consola.h"
 #include "consolaUtils.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 
 // ./consola 10000 /C:\Users\esposito\Carpetas Compartidas SO\TP\tp-2022-1c-Ubunteam\consola\instrucciones.txt
-     if(argc < 3) {
-        return EXIT_FAILURE;
+	if(argc < 3) {
+		return EXIT_FAILURE;
     }
 
-    int tamanio_proceso = atoi(argv[1]) ; 
-    int conexion_consola;
+	int tamanio_proceso = atoi(argv[1]) ;
+	int conexion_consola;
 
     t_log *logger;
     t_config *config;
@@ -42,14 +41,14 @@ int main(int argc, char **argv)
     terminar_programa(conexion_consola, logger, config);
 }
 
-t_log *iniciar_logger(void)
-{
+
+t_log *iniciar_logger(void){
+
     t_log *nuevo_logger;
 
     nuevo_logger = log_create("consola.log", "consola", 1, LOG_LEVEL_INFO);
 
-    if (nuevo_logger == NULL)
-    {
+    if (nuevo_logger == NULL){
         printf("No se pudo crear el logeer consola\n");
         exit(1);
     }
@@ -57,25 +56,22 @@ t_log *iniciar_logger(void)
     return nuevo_logger;
 }
 
-t_config *iniciar_config(void)
-{
+
+t_config *iniciar_config(void){
     t_config *nuevo_config;
 
     nuevo_config = config_create("cfg/consola.config");
 
-    if (nuevo_config == NULL)
-    {
-        printf("No se pudo leer la config\n");
+    if (nuevo_config == NULL){
+    	printf("No se pudo leer la config\n");
         exit(2);
     }
-
 
     return nuevo_config;
 }
 
 
-void paquete_proceso(int conexion)
-{
+void paquete_proceso(int conexion){
 
     // char *leido = readline("> ");
     //    char *leido = leer_archivo(split[2]);
@@ -84,73 +80,62 @@ void paquete_proceso(int conexion)
     t_paquete *paquete = crear_paquete();
 
     while (1) {
-    char* leido = leer_archivo();
+    	char* leido = leer_archivo();
 
-	char** split = string_split(leido, "\n");
+    	char** split = string_split(leido, "\n");
 
-    int longitud_instrucciones = string_array_size(split);
+    	int longitud_instrucciones = string_array_size(split);
 
-    if(string_contains(split[0], "NO_OP") ) {
-        			char** split_NO_OP = string_split(split[0], " ");
+    	if(string_contains(split[0], "NO_OP") ) {
+    		char** split_NO_OP = string_split(split[0], " ");
+    		int parametro_NO_OP = atoi(split_NO_OP[1]);
+            for(int i=0; i< parametro_NO_OP  ; i++){
+            	printf("NO_OP %d ", parametro_NO_OP);
+            }
+    	} else if (string_contains(split[1], "I/O")){
+    		char** split_IO = string_split(split[1], " ");
+            int parametro_IO = atoi(split_IO[1]);
+            printf("I/O %d ", parametro_IO);
 
-                    int parametro_NO_OP = atoi(split_NO_OP[1]);
-                    	for(int i=0; i< parametro_NO_OP  ; i++)
-					{
-						printf("NO_OP %d ", parametro_NO_OP);
+    	} else if (string_contains(split[2], "READ")){
+    		char** split_READ = string_split(split[2], " ");
+            int parametro_READ = atoi(split_READ[1]);
+            printf("READ %d ", parametro_READ);
 
-					}    
+    	} else if (string_contains(split[3], "WRITE")) {
+    		char** split_WRITE = string_split(split[3], " ");
+            int parametro1_WRITE = atoi(split_WRITE[1]);
+            int parametro2_WRITE = atoi(split_WRITE[2]);
+            printf("WRITE %d %d ", parametro1_WRITE,parametro2_WRITE);
 
-                
-    }else if (string_contains(split[1], "I/O")){
-                        
-                        	char** split_IO = string_split(split[1], " ");
-                            int parametro_IO = atoi(split_IO[1]);
-                            printf("I/O %d ", parametro_IO);
+    	} else if (string_contains(split[4], "COPY")){
+    		char** split_COPY = string_split(split[4], " ");
+            int parametro1_COPY = atoi(split_COPY[1]);
+            int parametro2_COPY = atoi(split_COPY[2]);
+            printf("COPY %d %d ", parametro1_COPY,parametro2_COPY);
 
-                    }else if (string_contains(split[2], "READ")){
-                            char** split_READ = string_split(split[2], " ");
-                            int parametro_READ = atoi(split_READ[1]);
-                            printf("READ %d ", parametro_READ);
+    	} else if (string_contains(split[5], "EXIT")){
 
-                   }else if (string_contains(split[3], "WRITE")) {
-                            char** split_WRITE = string_split(split[3], " ");
-                                int parametro1_WRITE = atoi(split_WRITE[1]);
-                                int parametro2_WRITE = atoi(split_WRITE[2]);
-                            printf("WRITE %d %d ", parametro1_WRITE,parametro2_WRITE);
-
-                   }else if (string_contains(split[4], "COPY")){
-                            	char** split_COPY = string_split(split[4], " ");
-                                int parametro1_COPY = atoi(split_COPY[1]);
-                                int parametro2_COPY = atoi(split_COPY[2]);
-
-                            printf("COPY %d %d ", parametro1_COPY,parametro2_COPY);
-
-                   }else if (string_contains(split[5], "EXIT")){
-
-                            printf("EXIT");
-                   }
+    		printf("EXIT");
+    	}
 
         agregar_a_paquete(paquete, split, longitud_instrucciones + 1);
         free(leido);
         free(split);
- }
-                   }
-
-                    }
+    }
     enviar_paquete(paquete, conexion);
     eliminar_paquete(paquete);
 }
 
-void terminar_programa(int conexion, t_log *logger, t_config *config)
-{
 
-    if (logger != NULL)
-    {
+
+void terminar_programa(int conexion, t_log *logger, t_config *config){
+
+    if (logger != NULL){
         log_destroy(logger);
     }
 
-    if (config != NULL)
-    {
+    if (config != NULL){
         config_destroy(config);
     }
 
@@ -158,32 +143,28 @@ void terminar_programa(int conexion, t_log *logger, t_config *config)
 }
 
 // // VERSION 1 - leer_archivo()
-  char *leer_archivo()
-  {
-        char cadena[100] ;
-     FILE *archivo, *salida ;
-     archivo = fopen("argv[2]", "r");
-     salida = fopen("instrucciones_parseadas.txt", "r");
-     if (archivo == NULL)
-      {
-          perror("Error al abrir el archivo");
-          return 1;
-     }
-  if (salida == NULL)
-      {
-          perror("Error al abrir el archivo");
-          return 1;
-     }
- while(fgets(cadena,100,archivo)) {
-     fputs(cadena,salida);
- }
-
-     printf("&s", cadena);
-     fclose(archivo);
-     fclose(salida);
-     printf("\n Se ha leido el archivo de pseudocodigo correctamente ..");
-     return cadena;
-  }
+char *leer_archivo(){
+	char cadena[100] ;
+	FILE *archivo, *salida ;
+	archivo = fopen("argv[2]", "r");
+	salida = fopen("instrucciones_parseadas.txt", "r");
+	if (archivo == NULL){
+        perror("Error al abrir el archivo");
+        return 1;
+	}
+	if (salida == NULL){
+        perror("Error al abrir el archivo");
+        return 1;
+	}
+	while(fgets(cadena,100,archivo)) {
+		fputs(cadena,salida);
+	}
+	printf("%s", cadena);
+	fclose(archivo);
+	fclose(salida);
+	printf("\n Se ha leido el archivo de pseudocodigo correctamente ..");
+	return cadena;
+}
 
 
 // VERSION 2 - leer_archivo()
