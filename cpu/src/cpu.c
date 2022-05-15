9e7fea089f319032d0278fc2619aa39de752bb22
Francisco Cicerchia <francicerchia@gmail.com>
2022-05-15 18:52:38 -0300

    Avance ciclo de instrucciones

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	    log_info(logger, "CPU listo para recibir al modulo cliente");
 	    int cliente_fd = esperar_cliente(server_fd);
 
-	    t_list *lista;
+	    t_list *lista = list_create();
 	    while (1)
 	    {
 	        int cod_op = recibir_operacion(cliente_fd);
@@ -40,3 +40,64 @@ void iterator(char *value)
     // printf("Valor: %s\n", value);
     log_info(logger, "%s", value);
 }
+
+//void fetch(t_pcb* pcb){
+//	t_ins* instruccionProxima = ;
+//	instruccionProxima = list_get(pcb->instrucciones,pcb->program_counter);
+//	decode(instruccionProxima);
+//	pcb->program_counter++;
+//	free(instruccionProxima);
+//}
+
+//void decode(t_ins* instruccion){
+
+//	if(string_contains(instruccion->tipoInstruccion,"NO_OP")){
+//		ejecutarNO_OP();
+//	}
+
+//	if(string_contains(instruccion->tipoInstruccion,"I/O")){
+//		ejecutarIO(instruccion->primerParametro);
+//	}
+
+//	if(string_contains(instruccion->tipoInstruccion,"READ")){
+//		ejecutarREAD(instruccion->primerParametro);
+//	}
+
+//	if(string_contains(instruccion->tipoInstruccion,"WRITE")){
+//		ejecutarWRITE(instruccion->primerParametro,instruccion->segundoParametro);
+//	}
+
+//	if(string_contains(instruccion->tipoInstruccion,"COPY")){
+
+//		fetchOperands(instruccion->segundoParametro);
+//		ejecutarCOPY(instruccion->primerParametro,instruccion->segundoParametro);
+//	}
+
+//	if(string_contains(instruccion->tipoInstruccion,"EXIT")){
+//		ejecutarEXIT();
+//	}
+
+//}
+
+void ejecutarNO_OP(){}
+
+void ejecutarIO(tiempo){
+
+}
+
+void ejecutarREAD(dirLogica){
+
+}
+
+void ejecutarWRITE(dirLogica,valor){
+
+}
+
+void ejecutarCOPY(dirLogicaDestino,dirLogicaOrigen){
+
+}
+
+void ejecutarEXIT(){
+
+}
+
