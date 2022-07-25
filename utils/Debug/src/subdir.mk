27b4a39d505b371ba88a196e6a60302609244eb6
Tomas Fernandez Francou <Tfernndezfrancou@frba.utn.edu.ar>
2022-07-25 04:33:19 -0300

    Agrego makefile faltante de utils

---
+++ utils/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/comunicacion.c \
+../src/log_config.c \
+../src/sockets.c 
+
+OBJS += \
+./src/comunicacion.o \
+./src/log_config.o \
+./src/sockets.o 
+
+C_DEPS += \
+./src/comunicacion.d \
+./src/log_config.d \
+./src/sockets.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/tp-2022-1c-Ubunteam/utils/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
