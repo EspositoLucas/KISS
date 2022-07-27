35b93fd38fd328a6a033964f7b23a21a41cddf2c
EspositoLucas <espositolucas2002@gmail.com>
2022-07-27 17:05:42 -0300

    Borro todos los AI_PASSIVE

--- utils/src/sockets.c
+++ utils/src/sockets.c
@@ -10,7 +10,6 @@ int crear_conexion(char *ip, char *puerto)
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
 
     getaddrinfo(ip, puerto, &hints, &server_info);
 
